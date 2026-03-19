1441fd138fab36c35b40fc268f59c3d4eea6a4a6
Roshini <roshinipidikiti@gmail.com>
2026-03-19 19:15:16 +0530

    Added control

--- p1.py
+++ p1.py
@@ -1,3 +1,4 @@
+import math
 # product catalog
 catalog={
     "E101": ("Laptop", 65000.0, 8),
@@ -8,6 +9,9 @@ catalog={
     "E106": ("Bluetooth Speaker", 1500.0, 30)
 
 }
+bookkeeping={
+    
+}
 
 def print_catalog():
     print("Catalog: ")
@@ -15,3 +19,54 @@ def print_catalog():
     for pid,(name,price,stock) in catalog.items():
         print(f"{pid:<6}{name:<22}${price:<11} {stock}")
 
+def place_order():
+    pid=input("Enter Product id: ").strip()
+    quantity=int(input("Enter the Quantity"))
+    if pid not in catalog:
+        print("Item do not exist")
+        return
+    
+    name,price,stock=catalog[pid]
+    if stock==0:
+        print("Stock not available")
+        return
+    if quantity>stock:
+        print("Quantity as requested is not available")
+        print("Available Quantity is: ",stock)
+        return
+    
+    totalamount=price*quantity
+    if totalamount>10000:
+        totalamount=(0.90*totalamount)
+
+    totalamount=math.ceil(totalamount*100)/100
+    catalog[pid]=(name,price,stock-quantity)
+    
+    rating =input("Please give us a rating on a scale of (1-5): ")
+
+
+    
+
+
+def partb():
+    while True:
+        print("Menu: ")
+        print("1. Display Product Catalog")
+        print("2. Place Order")
+        print("3. View Analytics Summary")
+        print("4. Save Order Report to File")
+        print("5. Exit")
+
+        choice=int(input("Enter your choice: "))
+        if choice==1:
+            print_catalog()
+        elif choice==2:
+            place_order()
+        elif choice==3:
+            analytics_summary()
+        elif choice==4:
+            save_order()
+        elif choice==5:
+            break
+        else:
+            print("Invalid")
